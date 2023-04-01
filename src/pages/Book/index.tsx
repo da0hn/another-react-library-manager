1b8e2100f788a98c348ce40811e029eee47ca819
Da0hn <gabrielhondacba@gmail.com>
2023-03-31 22:38:00 -0400

    feat: implement header and style in `book` page

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,8 +1,20 @@
-
-
+import logoImage from '../../assets/logo.svg'
 import './styles.css'
+import {Link} from "react-router-dom";
+import {FiPower} from "react-icons/fi";
 
 export default function Book() {
-    return <h1>Book</h1>
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Library Logo"/>
+                <span>Welcome, <strong>Gabriel</strong></span>
+                <Link className='button' to='/book/new'>Add New Book</Link>
+                <button type='button'>
+                    <FiPower size={18} color='#251fc5'></FiPower>
+                </button>
+            </header>
+        </div>
+    );
 }
 
