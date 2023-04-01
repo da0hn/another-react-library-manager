a87a76c15d421c8019ff9c96d0a453f54793ecd1
Da0hn <gabrielhondacba@gmail.com>
2023-03-31 22:17:46 -0400

    feat: create `Book` component and add to `AppRouter`

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,15 @@
 import Login from './pages/Login'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Book from "./pages/Book";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login/>}></Route>
+            </Routes>
+            <Routes>
+                <Route path='/books' element={<Book/>}></Route>
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/Book/index.tsx
@@ -0,0 +1,8 @@
+
+
+import './styles.css'
+
+export default function Book() {
+    return <h1>Book</h1>
+}
+
