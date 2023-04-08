4344106ee3f4a09c78d2f8f9e7b3446ddc32c97f
Da0hn <gabrielhondacba@gmail.com>
2023-04-08 09:45:28 -0400

    style: format code

--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -1,43 +1,43 @@
-import logoImage from '../../assets/logo.svg'
-import './styles.css'
-import {Link} from "react-router-dom";
-import {FiEdit, FiPower, FiTrash2} from "react-icons/fi";
+import logoImage from '../../assets/logo.svg';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import { FiEdit, FiPower, FiTrash2 } from 'react-icons/fi';
 
 export default function Books() {
-    return (
-        <div className="book-container">
-            <header>
-                <img src={logoImage} alt="Library Logo"/>
-                <span>Welcome, <strong>Gabriel</strong></span>
-                <Link className='button' to='/book/new'>Add New Book</Link>
-                <button type='button'>
-                    <FiPower size={18} color='#251fc5'></FiPower>
-                </button>
-            </header>
+  return (
+    <div className="book-container">
+      <header>
+        <img src={logoImage} alt="Library Logo"/>
+        <span>Welcome, <strong>Gabriel</strong></span>
+        <Link className="button" to="/book/new">Add New Book</Link>
+        <button type="button">
+          <FiPower size={18} color="#251fc5"></FiPower>
+        </button>
+      </header>
 
-            <h1>Registered Books</h1>
+      <h1>Registered Books</h1>
 
-            <ul>
-                <li>
-                    <strong>Title: </strong>
-                    <p>Docker Deep Dive</p>
-                    <strong>Author: </strong>
-                    <p>Nigel Poulton</p>
-                    <strong>Price: </strong>
-                    <p>R$ 47,90</p>
-                    <strong>Release Date: </strong>
-                    <p>12/07/2017</p>
+      <ul>
+        <li>
+          <strong>Title: </strong>
+          <p>Docker Deep Dive</p>
+          <strong>Author: </strong>
+          <p>Nigel Poulton</p>
+          <strong>Price: </strong>
+          <p>R$ 47,90</p>
+          <strong>Release Date: </strong>
+          <p>12/07/2017</p>
 
-                    <button type='button'>
-                        <FiEdit size={20} color='#251fc5'></FiEdit>
-                    </button>
-                    <button type='button'>
-                        <FiTrash2 size={20} color='#251fc5'></FiTrash2>
-                    </button>
-                </li>
-            </ul>
+          <button type="button">
+            <FiEdit size={20} color="#251fc5"></FiEdit>
+          </button>
+          <button type="button">
+            <FiTrash2 size={20} color="#251fc5"></FiTrash2>
+          </button>
+        </li>
+      </ul>
 
-        </div>
-    );
+    </div>
+  );
 }
 
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,21 @@
 import './styles.css';
 
 import logoImage from '../../assets/logo.svg';
-import padlock from "../../assets/padlock.png";
+import padlock from '../../assets/padlock.png';
 
 export default function Login() {
-    return (
-        <div className="login-container">
-            <section className="form">
-                <img src={logoImage} alt="Library Logo"/>
-                <form>
-                    <h1>Access your Account</h1>
-                    <input placeholder="Username" type="text"/>
-                    <input placeholder="Password" type="password"/>
-                    <button className='button' type="submit">Login</button>
-                </form>
-            </section>
-            <img src={padlock} alt="Login"/>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <section className="form">
+        <img src={logoImage} alt="Library Logo"/>
+        <form>
+          <h1>Access your Account</h1>
+          <input placeholder="Username" type="text"/>
+          <input placeholder="Password" type="password"/>
+          <button className="button" type="submit">Login</button>
+        </form>
+      </section>
+      <img src={padlock} alt="Login"/>
+    </div>
+  );
 }
