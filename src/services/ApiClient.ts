d29c48f9d12ea80df5b15f79572d01bca47a1d8b
Da0hn <gabrielhondacba@gmail.com>
2023-04-10 20:29:37 -0400

    feat: add header factory function

--- src/services/ApiClient.ts
+++ src/services/ApiClient.ts
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const apiClient = axios.create({
   baseURL: 'http://localhost:8080',
 });
+
+export const makeHeader = (accessToken: string) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
+};
