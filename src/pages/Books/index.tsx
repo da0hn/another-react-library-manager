ff6ac09abfb92b3124aa62f321c7a9325341c6c0
Da0hn <gabrielhondacba@gmail.com>
2023-04-10 20:31:50 -0400

    feat: add fetch for all `books` without pagination

--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -2,8 +2,23 @@ import logoImage from '../../assets/logo.svg';
 import './styles.css';
 import { Link } from 'react-router-dom';
 import { FiEdit, FiPower, FiTrash2 } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
+import { getVariable, StorageVariables } from '../../services/StorageService';
+import { BookItem, fetchBook } from '../../services/BookService';
 
 export default function Books() {
+  const [ books, setBooks ] = useState<BookItem[]>([]);
+
+  const accessToken = getVariable(StorageVariables.ACCESS_TOKEN);
+
+  useEffect(
+    () => {
+      fetchBook()
+        .then(data => setBooks(data));
+    },
+    [ accessToken ],
+  );
+
   return (
     <div className="book-container">
       <header>
@@ -18,23 +33,27 @@ export default function Books() {
       <h1>Registered Books</h1>
 
       <ul>
-        <li>
-          <strong>Title: </strong>
-          <p>Docker Deep Dive</p>
-          <strong>Author: </strong>
-          <p>Nigel Poulton</p>
-          <strong>Price: </strong>
-          <p>R$ 47,90</p>
-          <strong>Release Date: </strong>
-          <p>12/07/2017</p>
+        {
+          books.map((book) => (
+            <li key={book.id}>
+              <strong>Title: </strong>
+              <p>{book.title}</p>
+              <strong>Author: </strong>
+              <p>{book.author}</p>
+              <strong>Price: </strong>
+              <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(book.price)}</p>
+              <strong>Release Date: </strong>
+              <p>{Intl.DateTimeFormat('pt-BR').format(new Date(book.launchDate))}</p>
 
-          <button type="button">
-            <FiEdit size={20} color="#251fc5"></FiEdit>
-          </button>
-          <button type="button">
-            <FiTrash2 size={20} color="#251fc5"></FiTrash2>
-          </button>
-        </li>
+              <button type="button">
+                <FiEdit size={20} color="#251fc5"></FiEdit>
+              </button>
+              <button type="button">
+                <FiTrash2 size={20} color="#251fc5"></FiTrash2>
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
     </div>
--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,14 +1,40 @@
 import { getVariable, StorageVariables } from './StorageService';
-import { apiClient } from './ApiClient';
+import { apiClient, makeHeader } from './ApiClient';
 
-type CreateBookRequest = {
+export type CreateBookRequest = {
   title: string,
   author: string,
   launchDate: Date,
   price: number,
 }
 
+export type BookItem = {
+  id: number,
+  title: string,
+  author: string,
+  launchDate: Date,
+  price: number
+}
+
+export type FetchBookResponse = {
+  _embedded: {
+    bookVOes: BookItem[]
+  }
+  _links: {
+    self: {
+      href: string
+    }
+  }
+}
+
+
 export const createBook = async (request: CreateBookRequest) => {
   const accessToken = getVariable(StorageVariables.ACCESS_TOKEN);
-  await apiClient.post('/api/book/v1', request, { headers: { Authorization: `Bearer ${accessToken}` } });
+  await apiClient.post('/api/book/v1', request, makeHeader(accessToken!));
+};
+
+export const fetchBook = async (): Promise<BookItem[]> => {
+  const accessToken = getVariable(StorageVariables.ACCESS_TOKEN);
+  const { data } = await apiClient.get<FetchBookResponse>('/api/book/v1', makeHeader(accessToken!));
+  return data._embedded.bookVOes;
 };
